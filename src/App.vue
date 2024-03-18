c970a798c5af35bf3f60996fd4ba2b74127cda1a
JTS <jeroen_serdijn@hotmail.com>
2024-03-18 23:31:59 +0100

    add material ui to page.

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,7 @@
-<script setup></script>
+<script setup>
+import { RouterView } from "vue-router";
+</script>
 
 <template>
-  <div>
-    <a
-      href="https://www.instagram.com/releasetheriverofficial/"
-      target="_blank"
-    >
-      <img src="/logo.svg" class="logo" alt="Release the river logo" />
-    </a>
-  </div>
+  <router-view />
 </template>
-
-<style scoped>
-.logo {
-  color: #fff;
-  height: 20em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #fff);
-}
-</style>
