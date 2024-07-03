bee5f6cc1036cc9c69922337a8c83556d0440488
Jeroen Serdijn <jeroen_serdijn@hotmail.com>
2024-07-03 15:36:54 +0200

    change output to parent dir

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 const path = require("path");
 
 export default {
-  root: path.resolve(__dirname, "src"),
+  root: path.resolve(__dirname, "src"), // Make sure this points to the directory containing `index.html`
   build: {
-    outDir: "dist",
+    outDir: "../dist",
   },
   resolve: {
     alias: {
