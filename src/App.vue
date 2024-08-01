0e29e9daeea3114ea4ab57c8c53668e9377b07da
Jeroen Serdijn <jeroen_serdijn@hotmail.com>
2024-08-01 15:43:44 +0200

    initial start of presskit page.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <script setup>
 import Header from "./components/header/Header.vue";
-import NavBar from "./components/navbar/NavBar.vue";
 import Home from "./components/home/Home.vue";
-import About from "./components/about/About.vue";
-import Music from "./components/music/Music.vue";
-import Events from "./components/events/Events.vue";
-import Contact from "./components/contact/Contact.vue";
-import Footer from "./components/footer/Footer.vue";
 </script>
 
 <template>
   <main>
     <Header />
-    <NavBar />
     <Home />
-    <About />
-    <Music />
-    <Events />
-    <Contact />
   </main>
-  <footer>
-    <Footer />
-  </footer>
 </template>
-<style lang="scss" scoped></style>
